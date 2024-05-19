7ef62d6c55b618556396ca142fb0cdc382d64d3d
ramkumar gurav <ramkumarsgurav@gmail.com>
2024-05-19 22:42:34 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import "./globals.css";
+import { Poppins } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+import Providers from "./providers";
+
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: {
+    default: "Ramkumar",
+    template: "%s | Portfolio",
+  },
+  description:
+    "Welcome to Ramkumar's portfolio. Explore my projects, skills, and experience.",
+};
+
+const font = Poppins({ weight: "400", subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body
+        id="body"
+        className={` ${font.className} max-w-[1536px] mx-auto  ||  bg-white  dark:bg-black dark:text-white text-black`}
+      >
+        <Providers>
+          <Toaster position="bottom-right" toastOptions={{ duration: 3000 }} />
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
